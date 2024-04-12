a2fe99e448585cbc2154062bb02089cefa19470f
Phil Andreoli <phil.andreoli@gmail.com>
2024-04-12 22:03:02 +0000

    made changes to how the processing of data in order to answer questions regardless of the number of companies they ask about.

--- src/api/app/server.py
+++ src/api/app/server.py
@@ -1,16 +1,17 @@
-from os import getenv
-from fastapi import FastAPI
+from . import prompts 
 from fastapi.responses import RedirectResponse
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from langserve import add_routes
-from typing import List
+from os import getenv
 from langchain_openai import AzureChatOpenAI, AzureOpenAIEmbeddings
-from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import JsonOutputParser, StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnableLambda
 from langchain_community.vectorstores.azuresearch import AzureSearch, AzureSearchVectorStoreRetriever, Document
-from langchain_core.runnables import chain
 from operator import itemgetter
-from langchain.globals import set_debug
+from typing import List
+from langchain_core.runnables import chain
+from langserve import add_routes
 
 app = FastAPI(
     title="Financial Report Q&A API",
@@ -45,7 +46,7 @@ model: AzureChatOpenAI = AzureChatOpenAI(
     openai_api_version=open_ai_api_version,
     azure_deployment=open_ai_deployment_name,
     model=open_ai_model_version,
-    temperature=1,
+    temperature=0.1,
     api_key=open_ai_api_key
 )
 
@@ -66,69 +67,60 @@ vector_store: AzureSearch = AzureSearch(
 vector_store_retreiver: AzureSearchVectorStoreRetriever = AzureSearchVectorStoreRetriever(
     vectorstore=vector_store,
     search_type="hybrid",
-    k=15
+    k=3
 )
 
-parse_question_for_query_prompt = ChatPromptTemplate.from_template(
-    """
-    You are an AI assistant that's an expert in taking a user's question and doing some steps to get it ready for use in a RAG pattern.
-    Here are the steps that you need to follow:
-    1.  Find the company names that are in the user's question and determine their stock symbol.  
-    2.  Determine what financial report that they are looking for and the valid values are 10-K, 10-Q or not specified.   If the financial report is not specified, do not return anything. The financial report type will be put in a field called form_type
-    3.  Determine if they are looking for the most recent financial report.  If they are looking for the most recent report, then return true else return false.
-    4.  Determine what quarter they are looking for and then return the quarter number. 
-    5.  Determine what year are they are looking for and then return the year. 
-    6.  You should craft the filter that will be used in the search.  
-            Example 1:
-            search.in(stock_symbol, 'MSFT,NVDA', ',') and latest eq true
-            Example 2:
-            search.in(stock_symbol, 'MSFT,NVDA', ',') and latest eq false and year eq 2020 and cy_quarter eq 'Q2' and form_type eq '10-Q'
-            Example 3:
-            stock_symbol eq 'MSFT' and latest eq true 
-            Example 4:
-            stock_symbol eq 'MSFT' and latest eq false and cy_quarter eq 'Q2' and form_type eq '10-Q'
-    7.  You should only return a JSON object and nothing else that has a property called filter and another property called question.  The filter property will have the output from step 6 and the question property will have the question that the user asked.  The json object should look like the followng:
-
-    Question: {user_question}
-    """)
-
-    
-answer_question_with_context = ChatPromptTemplate.from_template(
-    """
-    You are an AI assistant that will answer the user's question fron the context that is provided to you.  Please include the source and the page number as a citation for the answers given.   
-    If you do not know the answer to the user's question, then return a message that says that you do not know the answer.
-
-    Context: {context}
-
-    Question: {user_question}
-    """)
-
-# Define the output parsers needed for the code
-filter_output_parser: JsonOutputParser = JsonOutputParser()
-answer_output_parser: StrOutputParser = StrOutputParser()
-
+# format all the documents in the list as a string that will be used as the
+# context to answer the question
 def format_docs(docs: List[Document]) -> str:
     return "\n\n".join(doc.page_content for doc in docs)
 
 
-# Create a custom chain that does the following
-# 1. Takes the user question and parses it to get the filter
-# 2. Queries the vector store to get the relavant content from the question
-# 3. Summarizes the content to answer the users question
-# 4. Returns the reponse to the user
+# From the original question, rephrase the question used to answer the question
+# regardless of the number of companies you want to know for.   
+rephrase_question_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", prompts.rephrase_prompt),
+        #MessagesPlaceholder("chat_history"),
+        ("human", "{input}")
+    ]
+)
+
+# For the rephrased question for each company that was identified, how should you answer
+# this is the prompt to help with answering the question
+qna_prompt = ChatPromptTemplate.from_template(prompts.qna_prompt)
+
+# This is the chain that is used to take the original question and break it apart into multiple questions
+# based on the number of companies in the original question. 
+rephraser_chain = (
+    {"input": itemgetter("input"), "chat_history": itemgetter("chat_history")}
+    | rephrase_question_prompt
+    | model
+    | JsonOutputParser()
+)
+
+#  This is the chain that is used to take each individual question and answer it with the context.  This chain is also
+#  used to summarize the answers and answer the original question
+qna_chain = (
+    { "question": itemgetter("question"), "context": itemgetter("context")}
+    | qna_prompt
+    | model
+    | StrOutputParser()
+)
+
+def process_questions(question) -> str:
+    documents_as_string = format_docs(vector_store_retreiver.invoke(input=question["question"], filters=question["filter"] ))
+    answer = qna_chain.invoke({"question": question["question"], "context": documents_as_string })
+    return answer 
+
 @chain
-def custom_chain(question: str) -> str:
-    prompt1 = parse_question_for_query_prompt.invoke({"user_question": question})
-    filter = model.invoke(prompt1)
-    filter_output = JsonOutputParser().invoke(filter)
-    context = format_docs(vector_store_retreiver.invoke(input=question, filters=filter_output["filter"] ))
-    chain2 : StrOutputParser = (
-        { "user_question": itemgetter("user_question"), "context": itemgetter("context")}
-        | answer_question_with_context
-        | model
-        | answer_output_parser
-    )
-    return chain2.invoke({"user_question": question, "context": context })
+def answer_financial_question_chain(question: str): 
+    all_answers = []
+    rephrased_output = rephraser_chain.invoke({"input": question, "chat_history": ""})
+    runnable = RunnableLambda(process_questions)
+    all_answers = runnable.batch(rephrased_output["output"])
+    final_answer = qna_chain.invoke({"question": rephrased_output["rephrasedQuestion"], "context": "\n\n".join(all_answers)})
+    return final_answer
 
 @app.get("/")
 async def redirect_root_to_docs():
@@ -136,7 +128,7 @@ async def redirect_root_to_docs():
 
 
 # Edit this to add the chain you want to add
-add_routes(app, custom_chain, path="/financial-report")
+add_routes(app, answer_financial_question_chain, path="/financialreports")
 
 if __name__ == "__main__":
     import uvicorn
